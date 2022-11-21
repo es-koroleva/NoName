b7ffe4cf58b6b849f821fb1a26d3df098793d219
es-koroleva <61079742+es-koroleva@users.noreply.github.com>
2022-11-21 20:57:34 +0300

    Urok_2

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-console.log('Hello World')
-console.log(325)
-console.log('Hello' + 'World')
-console.log(253+65)
+let number = '02';      // числовой тип
+console.log(number);
+
+let month = 'Февраль';
+console.log(month);        // Строчный тип
+
+let hello = `Добро ${"пожаловать"} ${"на курс,"} ${"Евгения"}`;
+console.log(hello);
+
+// alert("11.2022");
+
+document.getElementById("one").innerHTML = "Hello World!";
+
+typeof undefined;
+console.log(typeof undefined);                  // undefined
+typeof 0;
+console.log(typeof 0);                          // Number
+typeof 12n;
+console.log(typeof 12n);                        // BigInt
+typeof true
+console.log(typeof true);                       // Boolean
+typeof 'Hello';
+console.log(typeof 'Hello')                     // String
+typeof Symbol('id');
+console.log(typeof Symbol('id'))       // Symbol
+typeof null;
+console.log(typeof null);                        // Objects
+
