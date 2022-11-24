d4becf37f0502648804023ac18ed6c15d6e988c4
es-koroleva <61079742+es-koroleva@users.noreply.github.com>
2022-11-24 20:25:41 +0300

    Urok_3

--- main.js
+++ main.js
@@ -1,28 +1,76 @@
-let number = '02';      // числовой тип
-console.log(number);
+// УРОК 2
 
-let month = 'Февраль';
-console.log(month);        // Строчный тип
+// let number = '02';      // числовой тип
+// console.log(number);
+//
+// let month = 'Февраль';
+// console.log(month);        // Строчный тип
+//
+// let hello = `Добро ${"пожаловать"} ${"на курс,"} ${"Евгения"}`;
+// console.log(hello);
+//
+// // alert("11.2022");
+//
+// document.getElementById("one").innerHTML = "Hello World!>";
+//
+// typeof undefined;
+// console.log(typeof undefined);                  // undefined
+// typeof 0;
+// console.log(typeof 0);                          // Number
+// typeof 12n;
+// console.log(typeof 12n);                        // BigInt
+// typeof true
+// console.log(typeof true);                       // Boolean
+// typeof 'Hello';
+// console.log(typeof 'Hello')                     // String
+// typeof Symbol('id');
+// console.log(typeof Symbol('id'))                // Symbol
+// typeof null;
+// console.log(typeof null);                       // Objects
 
-let hello = `Добро ${"пожаловать"} ${"на курс,"} ${"Евгения"}`;
-console.log(hello);
+// УРОК 3
 
-// alert("11.2022");
+document.getElementById("one").innerHTML = "<h1>Hello World!</h1>";
+// let inputIN = document.querySelector(".text");
+// let btn = document.querySelector(".btn");
+// let div = document.querySelector(".out");
+// btn.onclick = function () {
+//     console.log("Da");
+//     console.log(inputIN.value);
+//     let e = inputIN.value;
+//     console.log(e);
+//     div.innerHTML = e;
+// };
 
-document.getElementById("one").innerHTML = "Hello World!";
+// let myBirthDay = prompt('Когда у тебя день рождения?');
+// console.log(myBirthDay);
+//
+// if (myBirthDay == 22) {
+//     alert("Да ты молод бро!");
+// } else if (myBirthDay == 23) {
+//     alert("Ты крут");
+// } else if (myBirthDay >= 33) {
+//     alert("От тебя уже веет старостью");
+// } else if (myBirthDay == 63) {
+//     alert("Тебе пора на пенсию");
+// } else if (myBirthDay == "") {
+//     alert("Введи что-нибудь");
+// } else {
+//     alert("Фууу, старик!");
+// }
 
-typeof undefined;
-console.log(typeof undefined);                  // undefined
-typeof 0;
-console.log(typeof 0);                          // Number
-typeof 12n;
-console.log(typeof 12n);                        // BigInt
-typeof true
-console.log(typeof true);                       // Boolean
-typeof 'Hello';
-console.log(typeof 'Hello')                     // String
-typeof Symbol('id');
-console.log(typeof Symbol('id'))       // Symbol
-typeof null;
-console.log(typeof null);                        // Objects
+let myBirthDay = prompt('Загадайте число от 1 до 200');
+
+if (myBirthDay > 100) {
+    document.querySelector(".out").innerHTML = myBirthDay;
+} else {
+    console.log(myBirthDay);
+}
+
+
+
+
+
+
+
 
